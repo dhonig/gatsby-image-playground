7a10e2365d5e4159dff4837d90fb9715dacdcfc4
kinduff <abarcadabra@gmail.com>
2018-07-30 14:19:52 -0500

    Uses backticks for values

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
 module.exports = {
     siteMetadata: {
-        title: 'Gatsby Default Starter',
+        title: `Gatsby Default Starter`,
     },
-    plugins: ['gatsby-plugin-react-helmet',
+    plugins: [`gatsby-plugin-react-helmet`,
         {
-            resolve: 'gatsby-source-filesystem',
+            resolve: `gatsby-source-filesystem`,
             options: {
-                name: 'data',
-                path: '${__dirname}/src/data/'
+                name: `data`,
+                path: `${__dirname}/src/data/`
             }
         },
         {
-            resolve: 'gatsby-source-filesystem',
+            resolve: `gatsby-source-filesystem`,
             options: {
-                name: 'img',
-                path: '${__dirname}/src/img/'
+                name: `img`,
+                path: `${__dirname}/src/img/`
             }
         },
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp'
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`
    ]
 }
